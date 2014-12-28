8e765729a9f041c4334c7ce877519a52c19b1750
Michael Perreux <mperreux@gmail.com>
2014-12-27 22:24:14 -0600

    fix contact form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config.js');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: config.user,
+    pass: config.pass,
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Michael Perreux' });
+});
+
+router.post('/contact', function(req, res) {
+  var mailOptions = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: config.user,
+    subject: 'Contact Form submission',
+    text: req.body.message
+  }
+  transporter.sendMail(mailOptions, function(error, info) {
+    if(error) {
+      console.log(error);
+      res.send('An error has occured');
+    }
+    else {
+      res.send('Form submitted sucessfully!')
+    }
+  });
 });
 
 module.exports = router;
